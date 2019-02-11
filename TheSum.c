1c616476fde4a04f2cb9dfa51c504dd706e0e771
Cristian-alfa <krisian.m01@gmail.com>
2019-02-11 11:33:14 +0100

    esercizio The Sum

---
+++ TheSum.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+FILE *f;
+int somma, num;
+
+f= fopen("sum.dat","r");
+if (!f){
+	fprintf(stdout, "file non esistente\n");
+	fprintf(stderr, "impossibile aprire il file\n");
+	return 0;
+}
+else{
+printf("Lettura dal file sum.dat in corso ...\n");
+fscanf(f, "%d", &somma);
+printf("Valore corrente: %d\n", somma);
+fclose(f);
+}
+
+printf("Inserisci il valore da sommare:");
+scanf("%d", &num);
+
+somma = somma+ num;
+
+printf("Nuovo valore: %d\n", somma);
+printf("Salvataggio nel file sum.dat in corso ...\n");
+
+fopen("sum.dat", "w");
+fprintf(f, "%d\n", somma);
+fclose(f);
+
+return 0;
+}
+
