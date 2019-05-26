e1850ef913577f18b3857953a124318cb972c2be
Cristian-alfa <krisian.m01@gmail.com>
2019-05-26 17:30:44 +0200

    creazione file e somma=0

--- TheSum.c
+++ TheSum.c
@@ -9,14 +9,15 @@ f= fopen("sum.dat","r");
 if (!f){
 	fprintf(stdout, "file non esistente\n");
 	fprintf(stderr, "impossibile aprire il file\n");
-	return 0;
+	somma = 0;
+	f= fopen("sum.dat","w");
+	fprintf(f, "%d\n", somma);
+	printf("Creazione file...\n");
 }
-else{
 printf("Lettura dal file sum.dat in corso ...\n");
 fscanf(f, "%d", &somma);
 printf("Valore corrente: %d\n", somma);
 fclose(f);
-}
 
 printf("Inserisci il valore da sommare:");
 scanf("%d", &num);
